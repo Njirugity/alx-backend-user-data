6fc5f6395c6fc63b3161518d4922bf0ce6af5a8f
brian <muriithibrian283@gmail.com>
2025-05-27 23:23:31 +0300

    Add the auth module

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,12 +13,42 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+
+
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def filter():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    if auth is None:
+        pass
+    excluded_paths = [
+                      '/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    authorization = auth.require_auth(request.path, excluded_paths)
+    if authorization:
+        if auth.authorization_header(request) is None:
+            abort(401, description="Unauthorized")
+        if auth.current_user(request) is None:
+            abort(403, description="Forbidden")
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -31,7 +61,8 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Forbidden handler
     """
-    return jsonify({"error":"Forbidden"}), 403
+    return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,9 +1,29 @@
+#!/usr/bin/env python3
+""" to manage the API authentication.
+"""
+
 from flask import request
 from typing import TypeVar, List, Iterable
 
 
 class Auth():
+    """
+    Template for all authentication system you will implement.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Check if authentication is required for the given path.
+
+        Args:
+            path (str): The path to check.
+            excluded_paths (List[str]): A list of paths
+            that are excluded from authentication.
+
+        Returns:
+            bool: True if authentication is required
+            False otherwise.
+        """
+
         if path is None and excluded_paths is None:
             return True
         elif not excluded_paths:
@@ -12,8 +32,35 @@ class Auth():
         normalized_excluded_paths = [ep.rstrip('/') for ep in excluded_paths]
 
         return normalized_path not in normalized_excluded_paths
-    
-    def authorization_header(self, request=None) -> str: 
-        return None
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns the authorization header for the given request.
+
+        Args:
+            request (Optional): The request object. Defaults to None.
+
+        Returns:
+            str: The authorization header.
+        """
+        if request is None:
+            return None
+        if 'Authorization' in request.headers:
+            return request.headers['Authorization']
+        else:
+            return None
+
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieve the current authenticated user.
+
+        Args:
+            request (Optional[Request]):
+            The request object representing
+            the current HTTP request.
+
+        Returns:
+            User: The current authenticated user.
+
+        """
         return None
